e61d0cc284dfeff9601393b252c105b243254d6d
rayliu7717 <rayliu2004@gmail.com>
2024-05-16 21:44:19 -0400

    Navigate programmatically

--- src/pages/ProtectedPage.js
+++ src/pages/ProtectedPage.js
@@ -1,15 +1,23 @@
 import React, { useEffect } from 'react';
-import { Navigate  } from 'react-router-dom';
+import { Navigate, useNavigate  } from 'react-router-dom';
 
 export const ProtectedPage = () => {
+
+    const isAuthed = false;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isAuthed) {
+            navigate('/');
+        }
+    });
     //return <h1>Only authed users should see this!</h1>;
 
-    const isAuthed = false
     // return isAuthed
     //     ? <h1>Only authed users should see this!</h1>
     //     : <Navigate to="/" />;
     //     ? <h1>Only authed users should see this!</h1>
-    return <Navigate  to="/" />;
+    //return <Navigate  to="/" />;
 
 }
     
